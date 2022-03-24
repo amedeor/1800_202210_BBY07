09ac72aaf97a37d158fdb8444234a20e38611759
pcammayo <pcammayo03@gmail.com>
2022-03-24 00:09:13 -0700

    Added account edit/save feature

--- scripts/account.js
+++ scripts/account.js
@@ -8,23 +8,46 @@ function populateAccountInfo() {
   auth.onAuthStateChanged(user => {
     if (user) {
       userInfo = db.collection("users").doc(user.uid);
-      
+
       userInfo.get()
         .then(userDoc => {
-          let name = userDoc.data().name;
-          let email = userDoc.data().email;
+          var userName = userDoc.data().name;
+          var userEmail = userDoc.data().email;
 
           let nameInput = document.querySelector("#name");
           let emailInput = document.querySelector("#email");
 
-          nameInput.setAttribute("value", name);
-          emailInput.setAttribute("value", email)
+          nameInput.setAttribute("value", userName);
+          emailInput.setAttribute("value", userEmail)
         });
-      }
+    }
   })
 }
 
 populateAccountInfo();
 
+const edit = document.getElementById("edit");
+const save = document.getElementById("save");
 
+edit.addEventListener("click", editUserInfo);
+save.addEventListener("click", saveUserInfo);
+
+function editUserInfo() {
+  document.getElementById("account-information-fields").disabled = false;
+}
+
+function saveUserInfo() {
+  userEmail = document.getElementById("email").value;
+  userName = document.getElementById("name").value;
+
+  userInfo.update({
+    name: userName,
+    email: userEmail
+  })
+  .then(() => {
+    console.log("Document successfully updated!");
+  })
+
+  document.getElementById('account-information-fields').disabled = true;
+}
 
